e761fd620e0cb844f6a33ac2db155948056917c4
HW-Lee <tony123930@gmail.com>
2015-03-05 16:09:04 +0800

    HW1 part1 completed: accuracy over 99%

--- HW01/Hw1PerceptronClassifier.m
+++ HW01/Hw1PerceptronClassifier.m
@@ -8,16 +8,31 @@ classdef Hw1PerceptronClassifier
     methods
         function obj = Hw1PerceptronClassifier(data)
             obj.w = data.w;
+            obj.b = data.b;
         end
         function y = predict(obj, X)
-            y = sign( X*obj.w );
+            y = sign( X*obj.w+obj.b );
         end
     end
     
     methods(Static = true)
         function obj = train(X, y)
-            data.w = [.4; -.3; -.7; .8];
+            data.w = zeros(size(X, 2), 1);
+            data.b = 0;
+            N = 1e2;
+            for ii = 1:N
+                errIdx = find( sign( X*data.w+data.b ) ~= y );
+                if isempty(errIdx)
+                    break;
+                end
+                data = Hw1PerceptronClassifier.updateParams(data, X(errIdx, :), y(errIdx));
+            end
             obj = Hw1PerceptronClassifier(data);
+        end
+        function [data_next] = updateParams(data, X_err, y_err)
+            eta = 1e-3;
+            data_next.w = data.w + eta*( sum( diag(y_err)*X_err, 1 )' );
+            data_next.b = data.b + eta*( sum( y_err ) );
         end
     end
     
--- HW01/Hw1_1Demo.m
+++ HW01/Hw1_1Demo.m
@@ -1,3 +1,4 @@
+Hw1_LinBinPerc_DataGen;
 clear all; close all;
 import Hw1PerceptronClassifier
 
@@ -8,8 +9,10 @@ y = [ones(length(class0), 1); -ones(length(class1), 1)];
 
 clear class0 class1;
 
+tic;
 clsfier = Hw1PerceptronClassifier.train(X, y);
+toc;
 y_predicted = clsfier.predict(X);
-accuracy = sum( y ~= y_predicted );
+accuracy = sum( y == y_predicted );
 accuracy = accuracy / length(y);
 display(accuracy);
