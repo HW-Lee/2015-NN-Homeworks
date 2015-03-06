1d58e82b666d8965f48786a426785a1342ed5bfb
HW-Lee <tony123930@gmail.com>
2015-03-06 14:56:35 +0800

    Added 'Parametrogram' with zero initialization

--- HW01/Hw1PerceptronClassifier.m
+++ HW01/Hw1PerceptronClassifier.m
@@ -3,12 +3,14 @@ classdef Hw1PerceptronClassifier
     properties
         w;
         b;
+        W_history;
     end
     
     methods
         function obj = Hw1PerceptronClassifier(data)
             obj.w = data.w;
             obj.b = data.b;
+            obj.W_history = data.W_history;
         end
         function y = predict(obj, X)
             y = sign( X*obj.w+obj.b );
@@ -16,21 +18,30 @@ classdef Hw1PerceptronClassifier
     end
     
     methods(Static = true)
-        function obj = train(X, y)
-            data.w = zeros(size(X, 2), 1);
-            data.b = 0;
+        function obj = train(X, y, initGuess)
+            if nargin >= 3
+                data.w = initGuess.w;
+                data.b = initGuess.b;
+            else
+                data.w = zeros(size(X, 2), 1);
+                data.b = 0;
+            end
             N = 1e2;
+            data.W_history = zeros(size(X, 2)+1, N+1);
+            data.W_history(:, 1) = [data.w; data.b];
             for ii = 1:N
                 errIdx = find( sign( X*data.w+data.b ) ~= y );
                 if isempty(errIdx)
                     break;
                 end
                 data = Hw1PerceptronClassifier.updateParams(data, X(errIdx, :), y(errIdx));
+                data.W_history(:, ii+1) = [data.w; data.b];
             end
             obj = Hw1PerceptronClassifier(data);
         end
         function [data_next] = updateParams(data, X_err, y_err)
             eta = 1e-3;
+            data_next = data;
             data_next.w = data.w + eta*( sum( diag(y_err)*X_err, 1 )' );
             data_next.b = data.b + eta*( sum( y_err ) );
         end
--- HW01/Hw1_1Demo.m
+++ HW01/Hw1_1Demo.m
@@ -16,3 +16,14 @@ y_predicted = clsfier.predict(X);
 accuracy = sum( y == y_predicted );
 accuracy = accuracy / length(y);
 display(accuracy);
+
+NIter = size(clsfier.W_history, 2)-1;
+for ii = 1:5
+    subplot(5, 1, ii);
+    plot(0:NIter , clsfier.W_history(ii, :));
+    if ii < 5
+        xlabel(['w_' num2str(ii)]);
+    else
+        xlabel('b');
+    end
+end
